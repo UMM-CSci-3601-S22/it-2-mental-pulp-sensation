6b1393d5aa2499feced19d470398c15cdfbd2fde
Conner <hetti031@morris.umn.edu>
2022-04-06 17:20:47 -0500

    Write e2e for changing product
    Cypress testing for changing each field on the product page through accessing the Edit Product dropdown. Some HTML changes were included to make the testing easier and to organize the HTML better.

--- client/cypress/support/product-profile.po.ts
+++ client/cypress/support/product-profile.po.ts
@@ -8,4 +8,22 @@ export class ProductProfilePage {
   getProductProfile() {
     return cy.get('app-product-card');
   }
+
+  changeCategory(option: string) {
+    cy.get('[data-test="openEditDropDown"]').click();
+    return cy.get('[data-test=categoryDropDown]').click().get(`[data-test=click${option}]`).click();
+  }
+
+  changeStore(option: string) {
+    cy.get('[data-test="openEditDropDown"]').click();
+    return cy.get('[data-test=storeDropDown]').click().get(`[data-test=click${option}]`).click();
+  }
+
+  submitChanges(){
+    return cy.get('[data-test="confirmChange"]').click();
+  }
+
+  grabCardInformation() {
+    return cy.get(`app-product-card .product-card`);
+  }
 }
