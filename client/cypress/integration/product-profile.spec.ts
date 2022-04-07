682f14e8daa4cc09eafbc5d5fa9784e453bd780a
Ollie Willette <wille667@morris.umn.edu>
2022-04-07 14:02:03 -0500

    Delete a test that tests a thing we don't have
    This test tested threshold filtering in products. We don't have threshold filtering in products.

--- client/cypress/integration/product-profile.spec.ts
+++ client/cypress/integration/product-profile.spec.ts
@@ -15,27 +15,4 @@ describe('Product profile' , () => {
   it('Should list product', () => {
     page.getProductProfile().get('.product-card-name').should('have.text', 'Sobe - Orange Carrot');
   });
-
-  it('Should type something in the threshold filter and have it return correct products', () => {
-    // Filter for the product with threshold of 3
-    cy.get('[data-test="thresholdInput"]').type('3');
-    // This makes sure the list of searched for items has loaded before
-    // we iterate through them in the next step. Hopefully this will
-    // resolve the detached DOM problem.
-    page.getProductProfile().should('have.length.at.least', 0);
-    // cy.wait(3000);
-    cy.get('[data-test="confirmChangeThreshold"]').click();
-    // This makes sure the list of searched for items has loaded before
-    // we iterate through them in the next step. Hopefully this will
-    // resolve the detached DOM problem.
-    page.getProductProfile().should('have.length.at.least', 0);
-
-    // All of the listed products should have the name we are filtering by
-
-    // page.getProductProfile().each(e => {
-    //   cy.wrap(e).find('[data-test="thresholdTest"]').should('have.text', 'Threshold: 3');
-    // });
-    page.getProductProfile().find('.product-card-threshold').each(el =>
-      expect(el.text()).to.equal('Threshold: 3'));
-  });
 });
